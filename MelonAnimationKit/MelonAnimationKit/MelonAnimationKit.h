46980059c1e9e4e7e8a1ad8dde5feedda1f041d0
Tomek Popis <tomek@melon-it.com>
2016-02-29 18:19:49 +0100

    Configure project

---
+++ MelonAnimationKit/MelonAnimationKit/MelonAnimationKit.h
@@ -0,0 +1,19 @@
+//
+//  MelonAnimationKit.h
+//  MelonAnimationKit
+//
+//  Created by Tomasz Popis on 29/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MelonAnimationKit.
+FOUNDATION_EXPORT double MelonAnimationKitVersionNumber;
+
+//! Project version string for MelonAnimationKit.
+FOUNDATION_EXPORT const unsigned char MelonAnimationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MelonAnimationKit/PublicHeader.h>
+
+
