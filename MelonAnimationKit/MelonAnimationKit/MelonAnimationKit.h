df7f350cff87792ef7233e50385ebd350dc85113
Tomek Popis <tomek@melon-it.com>
2021-02-11 12:22:42 +0100

    Fix - Access control

--- MelonAnimationKit/MelonAnimationKit/MelonAnimationKit.h
+++ MelonAnimationKit/MelonAnimationKit/MelonAnimationKit.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double MelonAnimationKitVersionNumber;
 //! Project version string for MelonAnimationKit.
 FOUNDATION_EXPORT const unsigned char MelonAnimationKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <MelonAnimationKit/PublicHeader.h>
+// In this header, you should import all the  headers of your framework using statements like #import <MelonAnimationKit/Header.h>
 
 
